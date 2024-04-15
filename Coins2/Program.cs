2ccb0f395ba10274aae5749d2f1fe5ec42ad5f6c
unknown <islammohamedahmed49@gmail.com>
2024-04-15 18:26:03 +0200

    c2

---
+++ Coins2/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+int n;
+string str = "";
+
+n = ToInt32(ReadLine());
+if(n == 1)
+{
+    str += $"{n}";
+}
+else
+{
+    str += $"{n} ";
+    while (n != 1)
+    {
+
+        for(int i = n - 1; i >= 1; i--)
+        {
+            if (n % i == 0)
+            {
+                str += $"{i} ";
+                n = i; break ;
+            }
+        }
+
+    }
+}
+
+WriteLine(str);
